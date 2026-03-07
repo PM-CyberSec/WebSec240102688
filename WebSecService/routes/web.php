5007a37c3f3a885866d3e10147b815ff49aa939a
omen212 <paulamagedcyber@gmail.com>
2026-03-07 21:49:18 +0000

    exercice1 finished

--- WebSecService/routes/web.php
+++ WebSecService/routes/web.php
@@ -3,15 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('layouts.master');
+});
+
+Route::get('/home', function () {
     return view('welcome');
 });
 
-Route::get('/multable', function () {
-    return view('multable'); //multable.blade.php
-});
 Route::get('/even', function () {
     return view('even'); //even.blade.php
 });
 Route::get('/prime', function () {
     return view('prime'); //prime.blade.php
+});
+
+Route::get('/multable/{j?}', function ($j = 5) {
+    $j = (int) $j;
+    return view('multable', compact('j')); //multable.blade.php
 });
